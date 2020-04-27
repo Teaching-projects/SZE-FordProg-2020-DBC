dfa2e87cbf19cfb5934d45533aaed7302b60cc71
handras <horvathandras159@gmail.com>
2020-04-27 22:10:10 +0200

    fixing grammar - left out new_symbols

--- pydbc/parser/parser.py
+++ pydbc/parser/parser.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from lark import Lark
 
 from pydbc.parser.dbc_transformer import DBC_Transformer
@@ -7,11 +9,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     print('creating parser...')
-    parser = Lark(open(r'grammar.lark'), start="dbc_file", lexer='standard',
-                  parser='lalr', transformer=DBC_Transformer, debug=True)
+    kwargs={
+        #"start" : "dbc_file",
+        "lexer" : 'contextual',
+        "parser" : 'lalr',
+        #"transformer" : DBC_Transformer,
+        "debug" : True
+    }
+    parser = Lark(open(r'grammar.lark'), **kwargs)
     print('done')
     print('parsing file...')
-    tree = parser.parse(open(r'../../example.dbc'))
+    tree = parser.parse(Path(__file__).parent.joinpath(r'../../test_data/example.dbc').open('r').read())
     print('done')
     print(tree.pretty())
 
